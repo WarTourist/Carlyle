d0c80f5090551533311a941e794d4b289aecc9db
WarTourist <merry.magdalene@gmail.com>
2016-07-21 13:35:13 -0700

    Add files via upload

---
+++ layercull.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class layercull : MonoBehaviour 
+{
+	public Camera localCamera;
+	public float crewCullDistance = 0; 
+	public float cannonCullDistance = 0; 
+	public float fireCullDistance = 0;
+	public float projectileCullDistance = 0;
+	public float debrisCullDistance = 0;
+	public float smokeSmallCullDistance = 0;
+	public float smokeCullDistance = 0;
+
+	void Start()
+	{
+		float [] distances = new float[32];
+		distances[8] = projectileCullDistance;
+		distances[9] = cannonCullDistance;
+		distances[11] = crewCullDistance;
+		distances[12] = fireCullDistance;
+		distances[18] = debrisCullDistance;
+		distances[19] = smokeSmallCullDistance;
+		distances[20] = smokeCullDistance;
+
+		localCamera.layerCullDistances = distances;
+	}
+}
---
+++ test.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour
+{
+	public Rigidbody localRigidbody;
+	public float time = 20.0f;
+
+	private Quaternion rotationStart;
+	private Quaternion rotationEnd;
+	private float elapsedTime = 0.0f;
+
+	void Start()
+	{
+		rotationStart = localRigidbody.rotation;
+		float tempY = localRigidbody.rotation.eulerAngles.y;
+		rotationEnd = Quaternion.AngleAxis(tempY + 90.0f, Vector3.up);
+		StartCoroutine(Rotate());
+	}
+
+	IEnumerator Rotate()
+	{
+		while(elapsedTime < time)
+		{
+			elapsedTime += Time.deltaTime;
+			localRigidbody.rotation = Quaternion.Slerp(rotationStart, rotationEnd, (elapsedTime / time));
+			yield return null;
+		}
+	}
+}
